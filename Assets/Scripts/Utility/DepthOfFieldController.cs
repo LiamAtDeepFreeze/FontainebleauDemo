52f872e15edb07cab577e91f93cb01eff1f2a99b
Liam Knightley <liam.knightley@gmail.com>
2020-06-05 01:49:28 -0700

    Fixed DoF effect. Started poly reduction

--- Assets/Scripts/Utility/DepthOfFieldController.cs
+++ Assets/Scripts/Utility/DepthOfFieldController.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.Serialization;
 
 public class DepthOfFieldController : MonoBehaviour
 {
+    [Serializable]
+    public enum RaycastMode
+    {
+        Raycast = 0,
+        SphereCast = 1
+    }
+    
     [SerializeField] private PostProcessProfile _profile;
+    [SerializeField] private VolumeProfile _volumeProfile;
     [SerializeField] private bool overrideAperature = true;
     [SerializeField] private float aperature = 5.4f;
+    [SerializeField] private RaycastMode mode;
+    [SerializeField] private float sphereCastRadius = 2f;
     [SerializeField] private float maxRange = 200f;
     [SerializeField] private float transitionSpeed = 1f;
     
@@ -19,10 +30,14 @@ public class DepthOfFieldController : MonoBehaviour
     private float _hitDistance;
 
     private DepthOfField _depthOfField;
+    private UnityEngine.Rendering.HighDefinition.DepthOfField _volumeDepthOfField;
 
     private void Start()
     {
-        _profile.TryGetSettings(out _depthOfField);
+        if (_profile != null)
+        {
+            _profile.TryGetSettings(out _depthOfField);
+        }
 
         if (_depthOfField != null)
         {
@@ -32,36 +47,67 @@ public class DepthOfFieldController : MonoBehaviour
                 _depthOfField.aperture.value = aperature;
             }
         }
+
+        if (_volumeProfile != null)
+        {
+            _volumeProfile.TryGet(out _volumeDepthOfField);
+        }
     }
 
     private void Update()
     {
-        _raycast = new Ray(transform.position, transform.forward * maxRange);
         _isHit = false;
-        if (Physics.Raycast(_raycast, out _raycastHit, maxRange))
+        _raycast = new Ray(transform.position, transform.forward * maxRange);
+        
+        switch (mode)
         {
-            _isHit = true;
-            _hitDistance = Vector3.Distance(transform.position, _raycastHit.point);
+            case RaycastMode.Raycast:
+                if (Physics.Raycast(_raycast, out _raycastHit, maxRange))
+                {
+                    _isHit = true;
+                    _hitDistance = Vector3.Distance(transform.position, _raycastHit.point);
+                }
+                else
+                {
+                    if (_hitDistance < maxRange)
+                    {
+                        _hitDistance++;
+                    }
+                }
+                break;
+            case RaycastMode.SphereCast:
+                if (Physics.SphereCast(_raycast, sphereCastRadius, out _raycastHit))
+                {
+                    _isHit = true;
+                    _hitDistance = Vector3.Distance(transform.position, _raycastHit.point);
+                }
+                else
+                {
+                    if (_hitDistance < maxRange)
+                    {
+                        _hitDistance++;
+                    }
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
         }
-        else
-        {
-            if (_hitDistance < maxRange)
-            {
-                _hitDistance++;
-            }
-        }
+        
         
         SetFocus();
     }
 
     private void SetFocus()
     {
-        if (_depthOfField == null)
+        if (_depthOfField != null)
         {
-            return;
+            _depthOfField.focusDistance.value = Mathf.Lerp(_depthOfField.focusDistance.value, _hitDistance, Time.deltaTime * transitionSpeed);
         }
-        
-        _depthOfField.focusDistance.value = Mathf.Lerp(_depthOfField.focusDistance.value, _hitDistance, Time.deltaTime * transitionSpeed);
+
+        if (_volumeDepthOfField != null)
+        {
+            _volumeDepthOfField.focusDistance.value = Mathf.Lerp(_volumeDepthOfField.focusDistance.value, _hitDistance, Time.deltaTime * transitionSpeed);
+        }
     }
 
     private void OnDrawGizmos()
