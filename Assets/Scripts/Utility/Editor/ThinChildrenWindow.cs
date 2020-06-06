d5666d73ff95ea7ac7b90aa0e328b404f835b6b8
Liam Knightley <liam.knightley@gmail.com>
2020-06-06 00:04:22 -0700

    Started optimizing main scene

---
+++ Assets/Scripts/Utility/Editor/ThinChildrenWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Utility.Editor
+{
+    public class ThinChildrenWindow : EditorWindow
+    {
+        private GameObject _selectedObject;
+        private int _childCount;
+
+        [MenuItem("Window/Tools/Children Thinner")]
+        public static void Initialize()
+        {
+            var window = GetWindow<ThinChildrenWindow>();
+            window.titleContent = new GUIContent("Thin Children");
+            window.Show();
+        }
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnSelectionChange()
+        {
+            
+        }
+
+        private void OnGUI()
+        {
+            
+        }
+    }
+}
